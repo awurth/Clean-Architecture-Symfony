f0d36d42a4675d67d5afcb6bfc12ed3a92ecff50
Alexis Wurth <awurth.dev@gmail.com>
2023-08-29 23:25:37 +0200

    Docker

--- config/services.php
+++ config/services.php
@@ -22,29 +22,29 @@ return static function (ContainerConfigurator $configurator) {
     $configurator->parameters()
         ->set('app.mailer.default_sender', '%env(MAILER_DEFAULT_SENDER)%');
 
-    $services->load('App\\Infrastructure\\Symfony\\', '../../../src/Infrastructure/Symfony/*')
+    $services->load('App\\Infrastructure\\Symfony\\', '../src/Infrastructure/Symfony/*')
         ->exclude([
-            '../../../src/Infrastructure/Symfony/{DependencyInjection,Kernel.php}',
-            '../../../src/Infrastructure/Symfony/Security/User.php'
+            '../src/Infrastructure/Symfony/{DependencyInjection,Kernel.php}',
+            '../src/Infrastructure/Symfony/Security/User.php'
         ]);
 
-    $services->load('App\\Presentation\\', '../../../src/Presentation/*');
+    $services->load('App\\Presentation\\', '../src/Presentation/*');
 
-    $services->load('App\\Application\\UseCase\\', '../../../src/Application/UseCase');
+    $services->load('App\\Application\\UseCase\\', '../src/Application/UseCase');
 
-    $services->load('App\\Infrastructure\\Persistence\\Doctrine\\Repository\\', '../../../src/Infrastructure/Persistence/Doctrine/Repository')
+    $services->load('App\\Infrastructure\\Persistence\\Doctrine\\Repository\\', '../src/Infrastructure/Persistence/Doctrine/Repository')
         ->tag('app.entity_repository');
 
-    $services->load('App\\Infrastructure\\Symfony\\Action\\', '../../../src/Infrastructure/Symfony/Action')
+    $services->load('App\\Infrastructure\\Symfony\\Action\\', '../src/Infrastructure/Symfony/Action')
         ->tag('controller.service_arguments');
 
-    $services->load('App\\Application\\CommandHandler\\', '../../../src/Application/CommandHandler')
+    $services->load('App\\Application\\CommandHandler\\', '../src/Application/CommandHandler')
         ->tag('messenger.message_handler', ['bus' => 'command.bus']);
 
-    $services->load('App\\Application\\QueryHandler\\', '../../../src/Application/QueryHandler')
+    $services->load('App\\Application\\QueryHandler\\', '../src/Application/QueryHandler')
         ->tag('messenger.message_handler', ['bus' => 'query.bus']);
 
-    $services->load('App\\Application\\EventHandler\\', '../../../src/Application/EventHandler')
+    $services->load('App\\Application\\EventHandler\\', '../src/Application/EventHandler')
         ->tag('messenger.message_handler', ['bus' => 'event.bus']);
 
     $services->alias(CommandBusInterface::class, CommandBus::class);
